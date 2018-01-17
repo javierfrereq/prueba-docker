b67031a950f9e45df570dee1b3d771b71b5d1431
Javier <javierfrereq@gmail.com>
2018-01-17 11:06:52 +0100

    modificar

--- contenedores/index.php
+++ contenedores/index.php
@@ -1,39 +1,39 @@
-<? php
-función  json_response ( $ mensaje  =  nulo , $ código  =  200 )
+
+function json_response($message = null, $code = 200)
 {
-    // borrar los viejos encabezados
-    header_remove ();
-    // establece el código real
-    http_response_code ( $ code );
-    // establece el encabezado para asegurarse de que el caché sea forzado
-    encabezado ( " Cache-Control: no-transform, public, max-age = 300, s-maxage = 900 " );
-    // trata esto como json
-    encabezado ( ' Content-Type: application / json ' );
-    $ status  =  array (
-        200  =>  ' 200 OK ' ,
-        400  =>  ' 400 Solicitud incorrecta ' ,
-        422  =>  ' Entidad no procesable ' ,
-        500  =>  ' 500 Error interno del servidor '
+ #clear the old headers
+    header_remove();
+  #set the actual code
+    http_response_code($code);
+   # set the header to make sure cache is forced
+    header("Cache-Control: no-transform,public,max-age=300,s-maxage=900");
+    # treat this as json
+    header('Content-Type: application/json');
+    $status = array(
+        200 => '200 OK',
+        400 => '400 Bad Request',
+        422 => 'Unprocessable Entity',
+        500 => '500 Internal Server Error'
         );
-    // ok, error de validación o error
-    encabezado ( ' Estado: ' . $ status [ $ code ]);
-    // devuelve el json codificado
-    return  json_encode ( array (
-        ' status '  =>  $ code  <  300 , // ¿ éxito o no?
-        ' mensaje '  =>  $ mensaje
+    # ok, validation error, or failure
+    header('Status: '.$status[$code]);
+    # return the encoded json
+    return json_encode(array(
+        'status' => $code < 300, // success or not?
+        'message' => $message
         ));
 }
-// si estás haciendo ajax con los encabezados application-json
-if ( empty ( $ _POST )) {
-    $ _POST  =  json_decode ( file_get_contents ( " php: // input " ), true )? : [];
+# if you are doing ajax with application-json headers
+if (empty($_POST)) {
+    $_POST = json_decode(file_get_contents("php://input"), true) ? : [];
 }
-// uso
-echo json_response ( 200 , ' trabajo ' ); // {"estado": verdadero, "mensaje": "trabajo"}
-// uso de la matriz
-echo json_response ( 200 , array (
-  ' data '  =>  array ( 1 , 2 , 3 )
+# usage
+echo json_response(200, 'working'); // {"status":true,"message":"working"}
+# array usage
+echo json_response(200, array(
+  'data' => array(1,2,3)
   ));
-// {"estado": verdadero, "mensaje": {"datos": [1,2,3]}}
-// uso con error
-echo json_response ( 500 , ' ¡Error del servidor! ¡Inténtalo de nuevo! ' ); // {"estado": falso, "mensaje": "¡Error del servidor! ¡Inténtalo de nuevo!"}
+# {"status":true,"message":{"data":[1,2,3]}}
+# usage with error
+echo json_response(500, 'Server Error! Please Try Again!'); # {"status":false,"message":"Server Error! Please Try Again!"}
 
