fd1ea366fed5cd767dffa3c69ba80d55dedee2b6
Javier <javierfrereq@gmail.com>
2018-01-18 02:42:43 +0100

    modificar dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,9 @@
-
 FROM php:7.0-apache
 
 MAINTAINER Freddy Javier Frere Quintero <javierfrereq@gmail.com>
-WORKDIR / app
 
-RUN apt-get update
+EXPOSE 80 
 
-EXPOSE 22 80 443 
+COPY contenedores/index.php /var/www/html/
 
-COPY contenedores/index.php /usr/local/etc/php
-COPY src/ /var/www/html/
-
-CMD ["index.php"]
 
--- contenedores/index.php
+++ contenedores/index.php
@@ -1,16 +1,27 @@
 <?php
-@session_start();
-require 'Slim/Slim.php';
-\Slim\Slim::registerAutoloader();
-app = new \Slim\Slim();
-app->config(array(
-    'templates.path' => 'vistas',
-));
-app->contentType('text/html; charset=utf-8');
-app->get('/', function() {
-            echo "Pagina de gestión API REST de mi aplicación.";
-        });
-app->get('/status', function() {
-            echo json_encode(array('status'=>'ok'));
-        });
-?>
+function json_response($message = null, $code = 200)
+{
+       header_remove();
+       http_response_code($code);
+        header("Cache-Control: no-transform,public,max-age=300,s-maxage=900");
+        header('Content-Type: application/json');
+    $status = array(
+        200 => '200 OK',
+        400 => '400 Bad Request',
+        422 => 'Unprocessable Entity',
+        500 => '500 Internal Server Error'
+        );
+    header('Status: '.$status[$code]);
+       return json_encode(array(
+        'status' => $code < 300, 
+        'message' => $message
+        ));
+}
+if (empty($_POST)) {
+    $_POST = json_decode(file_get_contents("php://input"), true) ? : [];
+}
+echo json_response(200, 'working');
+echo json_response(200, array(
+  'data' => array(1,2,3)
+  ));
+echo json_response(500, 'Server Error! Please Try Again!'); 
